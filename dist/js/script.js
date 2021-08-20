9d05db6932a7afe0e31fdb51d338076034c2aa2e
TatyanaKonina <tdkonina@edu.hse.ru>
2021-08-20 18:23:42 +0300

    menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,5 +16,14 @@ window.onscroll = function() {
     document.querySelector(".sidepanel").style.display = 'flex';
   }
 };
+const hamburger = document.querySelector('.hamburger'), menu = document.querySelector('.menu'), closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', ()=>{
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', ()=>{
+    menu.classList.remove('active');
+});
 
 
