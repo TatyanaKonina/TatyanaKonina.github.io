9b5ca19aeb28ee464e2c6aad1e752cbb8d7f8408
TatyanaKonina <tdkonina@edu.hse.ru>
2021-08-22 20:46:38 +0300

    smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -27,4 +27,10 @@ closeElem.addEventListener('click', ()=>{
     menu.classList.remove('active');
 });
 
+$("a[href^='#']").click(function(){
+  var _href = $(this).attr("href");
+  $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+  return false;
+});
+
 
