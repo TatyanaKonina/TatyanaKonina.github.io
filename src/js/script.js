8fdfc465a12a7904dc1ea396d5a90b697376fcb2
TatyanaKonina <tdkonina@edu.hse.ru>
2021-08-24 08:36:05 +0300

    smtp

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("pageup").classList.remove('hide');
@@ -10,27 +10,42 @@ window.onscroll = function() {
   let promoH = document.querySelector('.promo').offsetHeight - 150;
   let screenW = document.querySelector('.promo').clientWidth;
   console.log(screenW);
-  if (currentScrollPos < promoH || screenW < 1400){
+  if (currentScrollPos < promoH || screenW < 1400) {
     document.querySelector(".sidepanel").style.display = 'none';
-  }
-  else{
+  } else {
     document.querySelector(".sidepanel").style.display = 'flex';
   }
 };
-const hamburger = document.querySelector('.hamburger'), menu = document.querySelector('.menu'), closeElem = document.querySelector('.menu__close');
+const hamburger = document.querySelector('.hamburger'),
+  menu = document.querySelector('.menu'),
+  closeElem = document.querySelector('.menu__close');
 
-hamburger.addEventListener('click', ()=>{
-    menu.classList.add('active');
+hamburger.addEventListener('click', () => {
+  menu.classList.add('active');
 });
 
-closeElem.addEventListener('click', ()=>{
-    menu.classList.remove('active');
+closeElem.addEventListener('click', () => {
+  menu.classList.remove('active');
 });
 
-$("a[href^='#']").click(function(){
+$("a[href^='#']").click(function () {
   var _href = $(this).attr("href");
-  $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+  $("html, body").animate({
+    scrollTop: $(_href).offset().top + "px"
+  });
   return false;
 });
 
+$('form').submit(function (e) {
+  e.prventDefault();
+  $.ajax({
+    type: 'POST',
+    url: 'dist/mailer/smart.php',
+    data: $(this).serialize()
+  }).done(function () {
+    $(this).find('input').val('');
 
+    $('form').trigger('reset');
+  });
+  return false;
+});
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -1,7 +1,7 @@
 <?php 
 
 $name = $_POST['name'];
-$phone = $_POST['phone'];
+$message = $_POST['text'];
 $email = $_POST['email'];
 
 require_once('phpmailer/PHPMailerAutoload.php');
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'portfolio_mail@mail.ru';                 // Наш логин
+$mail->Password = 'xfcs2020';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('', 'Portfolio');   // От кого письмо 
+$mail->addAddress('tdkonina@edu.hse.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -32,7 +32,7 @@ $mail->Subject = 'Данные';
 $mail->Body    = '
 		Пользователь оставил данные <br> 
 	Имя: ' . $name . ' <br>
-	Номер телефона: ' . $phone . '<br>
+	Сообщение: ' . $message . '<br>
 	E-mail: ' . $email . '';
 
 if(!$mail->send()) {
